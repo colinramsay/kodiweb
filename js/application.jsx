0814425928f7ee2973cae0c6fb10651bcbbe0194
Colin Ramsay <colinramsay@gmail.com>
2014-10-17 23:10:22 +0100

    Simplify entry point main.jsx

--- js/application.jsx
+++ js/application.jsx
@@ -1,27 +1,2 @@
 /** @jsx React.DOM */
 
-var React = require('react'),
-    Status = require('./status'),
-    Loading = require('./loading'),
-    Menu = require('./menu'),
-    Flux = require('./flux');
-
-require('../css/main.css');
-
-module.exports = React.createClass({
-    
-    componentDidMount: function() {     
-        Flux.actions.control.initialize();
-    },
-
-
-    render: function() {
-        var active = this.props.activeRouteHandler();
-        return <div>
-            <Loading />
-            <Status />
-            {active}
-            <Menu />
-        </div>
-    }
-});
--- js/main.jsx
+++ js/main.jsx
@@ -1,24 +1,46 @@
 /** @jsx React.DOM */
 
 var React = require('react'),
-    Application = require('./application'),
-    Albums = require('./albums');
+    Flux = require('./flux');
 
 var Router = require('react-router'),
     Route = Router.Route,
     Routes = Router.Routes,
     DefaultRoute = Router.DefaultRoute;
 
-function onError(error) {
-    console.error(error);
-}
+var Status = require('./status'),
+    Loading = require('./loading'),
+    Menu = require('./menu'),
+    Albums = require('./albums');
+
+require('../css/main.css');
+
+
+var Application = React.createClass({
+    
+    componentDidMount: function() {     
+        Flux.actions.control.initialize();
+    },
+
+    render: function() {
+        var active = this.props.activeRouteHandler();
+        return <div>
+            <Loading />
+            <Status />
+            {active}
+            <Menu />
+        </div>
+    }
+});
+
 
 var routes = (
-  <Routes>
-    <Route handler={Application}>
-      <DefaultRoute handler={Albums}/>
-    </Route>
-  </Routes>
+    <Routes>
+        <Route handler={Application}>
+            <DefaultRoute handler={Albums}/>
+        </Route>
+    </Routes>
 );
 
-React.renderComponent(routes, document.getElementsByClassName('media')[0]);
+
+React.renderComponent(routes, document.getElementById('app-root'));
